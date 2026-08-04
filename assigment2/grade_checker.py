b774f9655c2822df28d4fdb39e4cd8770f355cd4
RAJESH REDDY <122891872+RajeshReddy999@users.noreply.github.com>
2026-08-04 18:19:56 +0530

    Add grade checker functionality

---
+++ assigment2/grade_checker.py
@@ -0,0 +1,9 @@
+print("Welcome to grade checker")
+
+score = int(input("enter your score"))
+
+if score > 90 : print("You got A grade")
+elif (score >= 80 and score <= 89) : print("You got B grade")
+elif (score >= 70 and score <= 79) : print("You got C grade")
+elif (score >= 60 and score <= 69) : print("You got D grade")
+elif (score <= 60) : print("You got F grade")
